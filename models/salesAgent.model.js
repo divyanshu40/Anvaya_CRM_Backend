e620b197278041288a93e33a1f1cfdbabe7526f9
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-06-26 16:04:55 +0530

    salesAgent.model.js file updated

--- models/salesAgent.model.js
+++ models/salesAgent.model.js
@@ -14,6 +14,6 @@ const SalesAgentSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const salesAgent = mongoose.model("SalesAgent", SalesAgentSchema);
+const salesAgent = mongoose.model("salesAgent", SalesAgentSchema);
 
 module.exports = { salesAgent };
